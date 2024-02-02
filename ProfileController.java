52b46aa848fd44f59148e03a44bd1a37eccb782c
Elif Tuğçe Neziroğlu <tugce.neziroglu@ug.bilkent.edu.tr>
2024-02-02 09:52:04 +0300

    Logout is functional now
    Logout button in the Profile page is functional now, however it requires improvements since not the main page, but just the profile page is closed when clicked.

--- ProfileController.java
+++ ProfileController.java
@@ -1,5 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -7,8 +8,21 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ProfileController {
+
+    private Scene scene;
+    private Stage stage;
+
+    @FXML
+    private Button logoutButton;
+
 
     @FXML
     private Button changePasswordButton;
@@ -82,6 +96,16 @@ public class ProfileController {
             applyChangesLabel.setTextFill(Color.color(0, 1, 0));
             applyChangesLabel.setText("Successfully Changed");
         }
+    }
+
+    @FXML
+    void logoutButtonListener(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/LoginPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
     }
 
 }
